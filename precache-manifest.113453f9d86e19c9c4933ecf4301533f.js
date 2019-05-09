eb14c73374dab2635783eb1a55a633e8409d0643
luvisoniasty <luvisonx@gmail.com>
2019-05-09 14:20:17 +0200

    Updates

---
+++ precache-manifest.113453f9d86e19c9c4933ecf4301533f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc7d9f6713ea45d227fef4414d225aec",
+    "url": "/crypto-calc/index.html"
+  },
+  {
+    "revision": "d5094a6828be1a884cc6",
+    "url": "/crypto-calc/static/js/2.4827c0d9.chunk.js"
+  },
+  {
+    "revision": "c9d38609d75f1e39eec9",
+    "url": "/crypto-calc/static/js/main.ed35485e.chunk.js"
+  },
+  {
+    "revision": "b81faa815a035003e46f",
+    "url": "/crypto-calc/static/js/runtime~main.fee3e68f.js"
+  },
+  {
+    "revision": "93363dd2c5d9d91aaf0164ee3d60f386",
+    "url": "/crypto-calc/static/media/background.93363dd2.webp"
+  }
+]);
